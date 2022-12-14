79ec423d19ae9120a9fbea3c0622adb5f5fc9d23
End <end@3nd.io>
2022-12-14 15:30:51 +0100

    add sformat argparse and fix parsing of the long sformat files

--- sformat.py
+++ sformat.py
@@ -3,6 +3,11 @@
 import sys
 import pdb
 
+import sys
+import os
+import pathlib
+import argparse
+
 from cons_utils import *
 from cons_xml import *
 
@@ -15,22 +20,53 @@ SFormat = Struct(
     "samplerate" / Int16ub,
     "frameCount" / Int32ul,
     "length" / Int32ul,
-    "essLength" / Int32ul,
-    "essUnk2" / Int32ul,
+    "sformatLength" / Int32ul,
+    "sformatUnk2" / Int32ul,
     "frameCount2" / Int32ul,
+    Probe(),
     "data" / If(lambda ctx: ctx.isShort == "false",
                Struct(
                    "unkCount" / Int32ul,
                    "unkX" / Int16ul,
                    "unkY" / Const(10, Int8ul),
                    "unkZ" / Const(5, Int8ul),
-                   "data" / Bytes(lambda ctx: ctx._.length * 5),
+                   "data" / Array(this.unkCount, "item" / Struct("data" / Array(5, Int8ul))),
                    )
                ),
     )
 
 if __name__ == "__main__":
-    f = open(sys.argv[1], "rb")
-    data = f.read()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--pack", action="store_true")
+    parser.add_argument("inputs", type=pathlib.Path, nargs='+', help="path to the input directory (pack) or file (unpack)")
+    parser.add_argument("-o", "--output", type=pathlib.Path, default="./out/", help="path to the output directory (unpack) / file (pack)")
+    args = parser.parse_args()
 
-    d = SFormat.parse(data)
+    for input in args.inputs:
+        f = open(sys.argv[1], "rb")
+        data = f.read()
+        f.close()
+
+        if not args.pack:
+            sys.stderr.write("parsing sformat...\n")
+            sformat = SFormat.parse(data)
+            sys.stderr.write("generating xml...\n")
+            xml = SFormat.toET(sformat, name="SFormat", is_root=True)
+            sys.stderr.write("indenting xml...\n")
+            ET.indent(xml, space="  ", level=0)
+            str = ET.tostring(xml).decode("utf-8")
+            sys.stderr.write("writing xml...\n")
+            f = open(os.path.join(args.output, f"{os.path.basename(input)}.xml"), "wb")
+            f.write(str.encode("utf-8"))
+            f.close()
+        else:
+            assert(str(input).endswith(".sformat.xml"))
+            xml = ET.fromstring(data.decode("utf-8"))
+            sys.stderr.write("rebuilding from xml...\n")
+            xml_rebuild, size, _ = SFormat.fromET(xml, "SFormat", is_root=True)
+            sys.stderr.write("building sformat...\n")
+            rebuilt_data = SFormat.build(xml_rebuild)
+            sys.stderr.write("writing sformat...\n")
+            f = open(os.path.join(args.output, f"{os.path.basename(str(input)[:-4])}"), "wb")
+            f.write(rebuilt_data)
+            f.close()

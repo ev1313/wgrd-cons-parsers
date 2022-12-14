1398fbc46525359d46a4bfae1785082494cfaa41
End <end@3nd.io>
2022-12-14 14:46:56 +0100

    add simple cmd line to dic.py

--- dic.py
+++ dic.py
@@ -4,6 +4,8 @@ import pdb
 
 import os
 import sys
+import argparse
+import pathlib
 from io import BytesIO
 
 from cons_utils import *
@@ -22,29 +24,37 @@ Dic = Struct(
 
 
 if __name__ == "__main__":
-    f = open(sys.argv[1], "rb")
-    data = f.read()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--pack", action="store_true")
+    parser.add_argument("inputs", type=pathlib.Path, nargs='+', help="path to the input directory (pack) or file (unpack)")
+    parser.add_argument("-o", "--output", type=pathlib.Path, default="./out/", help="path to the output directory (unpack) / file (pack)")
+    args = parser.parse_args()
 
-    dic = Dic.parse(data)
-    xml = Dic.toET(dic, name="Dic", is_root=True)
-    sys.stderr.write("indenting xml...\n")
-    ET.indent(xml, space="  ", level=0)
-    sys.stderr.write("finished indenting xml...\n")
-    str = ET.tostring(xml).decode("utf-8")
-    sys.stderr.write("finished generating xml...\n")
-    f = open("out/dic.xml", "wb")
-    f.write(str.encode("utf-8"))
-    f.close()
-    print(str)
+    for input in args.inputs:
+        f = open(sys.argv[1], "rb")
+        data = f.read()
+        f.close()
 
-    sys.stderr.write("test building xml...\n")
-    x, size, _ = Dic.fromET(xml, "Dic", is_root=True)
-    sys.stderr.write("built test xml...\n")
-
-    rebuild = Dic.build(x)
-
-    f = open("out/dic.dic", "wb")
-    f.write(rebuild)
-    f.close()
-
-    assert(rebuild == data)
+        if not args.pack:
+            sys.stderr.write("parsing dic...\n")
+            dic = Dic.parse(data)
+            sys.stderr.write("generating xml...\n")
+            xml = Dic.toET(dic, name="Dic", is_root=True)
+            sys.stderr.write("indenting xml...\n")
+            ET.indent(xml, space="  ", level=0)
+            str = ET.tostring(xml).decode("utf-8")
+            sys.stderr.write("writing xml...\n")
+            f = open(os.path.join(args.output, f"{os.path.basename(input)}.xml"), "wb")
+            f.write(str.encode("utf-8"))
+            f.close()
+        else:
+            assert(str(input).endswith(".dic.xml"))
+            xml = ET.fromstring(data.decode("utf-8"))
+            sys.stderr.write("rebuilding from xml...\n")
+            xml_rebuild, size, _ = Dic.fromET(xml, "Dic", is_root=True)
+            sys.stderr.write("building dic...\n")
+            rebuilt_data = Dic.build(xml_rebuild)
+            sys.stderr.write("writing dic...\n")
+            f = open(os.path.join(args.output, f"{os.path.basename(str(input)[:-4])}"), "wb")
+            f.write(rebuilt_data)
+            f.close()

5baf13ad7501d204bce143b479cbccfdd494258d
Tim Blume <tbl@sevenstax-intern.de>
2023-09-09 18:11:16 +0200

    version bump

---
+++ wgrd_cons_parsers/__init__.py
@@ -0,0 +1,4 @@
+from wgrd_cons_parsers.version import *
+
+__author__ = "Tim Blume <wgrd@3nd.io>"
+__version__ = version_string
---
+++ wgrd_cons_parsers/version.py
@@ -0,0 +1,3 @@
+version = (0, 1, 1)
+version_string = "0.1.1"
+release_date = "2023.09.15"

8b7a1196d5ad0bec71724807af95c4595951cdd2
Tim Blume <tbl@sevenstax-intern.de>
2023-09-14 02:42:34 +0200

    dic int to bytes

--- src/wgrd_cons_parsers/dic.py
+++ src/wgrd_cons_parsers/dic.py
@@ -9,7 +9,7 @@ Dic = Struct(
     "magic" / Const(b"TRAD"),
     "count" / Rebuild(Int32ul, len_(this.entries)),
     "entries" / Array(this.count, "Entry" / Struct(
-        "hash" / Int64ul,
+        "hash" / Bytes(8),
         "offset" / Rebuild(Int32ul, lambda ctx: ctx._._endoffset_entries + sum([x["_ptrsize_string"] for x in ctx._.entries[0:ctx._index]])),
         "length" / Rebuild(Int32ul, lambda ctx: len(ctx.string)),
         "string" / Pointer(this.offset, PaddedString(this.length*2, "utf-16-le")),
--- src/wgrd_cons_parsers/version.py
+++ src/wgrd_cons_parsers/version.py
@@ -1,3 +1,3 @@
-version = (0, 2, 0)
-version_string = "0.2.0"
+version = (0, 2, 1)
+version_string = "0.2.1"
 release_date = "2023.09.15"

bf6731357c303ccde7172335599a8e6e00b0c91a
Tim Blume <tbl@sevenstax-intern.de>
2023-09-16 14:32:52 +0200

    add rebuilding of ppk files, that's not correct yet. WGRD has duplicate detection - need to implement hashing on the data + reorder based on the paths and then calculate offsets / sizes based on the order of the paths + whether the hashed data was already handled

--- src/wgrd_cons_parsers/ppk.py
+++ src/wgrd_cons_parsers/ppk.py
@@ -3,12 +3,13 @@
 from .common import *
 
 from dingsda import *
+from dingsda.helpers import *
 from dingsda.string import *
 
 PPKTexture = Struct(
     "id" / Bytes(8),
-    "offset" / Int32ul,
-    "size" / Int32ul,
+    "offset" / Rebuild(Int32ul, lambda ctx: sum([x.size for x in ctx._.textures[0:ctx._index]])),
+    "size" / Rebuild(Int32ul, lambda ctx: len(ctx.tgvdata)),
     "xea" / Int16ul,
     "xeb" / Int16ul,
     "xf" / Const(0, Int32ul),
@@ -27,16 +28,16 @@ PPK = Struct(
     "size" / Int32ul,
     "checksum" / Bytes(16),
     "unkBoffset" / Const(0x40, Int32ul),
-    "unkBsize" / Int32ul,
+    "unkBsize" / Rebuild(Int32ul, lambda ctx: 24*len(ctx.textures)),
     "tex_offset" / Rebuild(Int32ul, lambda ctx: ctx.unkBoffset + ctx.unkBsize),
     Check(lambda ctx: ctx.tex_offset == ctx.unkBoffset + ctx.unkBsize),
-    "tex_size" / Int32ul,
-    "tex_count" / Int32ul,
+    "tex_size" / Rebuild(Int32ul, lambda ctx: sum([x.size for x in ctx.textures])),
+    "tex_count" / Rebuild(Int32ul, lambda ctx: len(ctx.textures)),
     "textures" / Area("Texture" / PPKTexture, this.unkBoffset, this.unkBsize),
     "paths_offset" / Rebuild(Int32ul, lambda ctx:ctx.tex_offset + ctx.tex_size),
     Check(lambda ctx: ctx.paths_offset == ctx.tex_offset + ctx.tex_size),
-    "paths_size" / Int32ul,
-    "paths_count" / Int32ul,
+    "paths_size" / Rebuild(Int32ul, lambda ctx: ctx.paths_count * (8+0x100)),
+    "paths_count" / Rebuild(Int32ul, this.tex_count),
     Check(lambda ctx: ctx.paths_size == ctx.paths_count * (8+0x100)),
     Check(lambda ctx: ctx.paths_count == ctx.tex_count),
     "paths" / Area("Path" / PPKPath, this.paths_offset, this.paths_size),

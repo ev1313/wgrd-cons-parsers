3515c9bc9778c15240fcae80fc013050ab262fa7
Tim Blume <tbl@sevenstax-intern.de>
2023-09-16 12:22:36 +0200

    fix rebuild of tgv files

--- src/wgrd_cons_parsers/tgv.py
+++ src/wgrd_cons_parsers/tgv.py
@@ -5,6 +5,8 @@ from .common import *
 from dingsda import *
 from dingsda.string import *
 
+from .utils import align
+
 TGV = Struct(
     "version" / Int32ul,
     "compressed" / Int32ul,
@@ -15,9 +17,9 @@ TGV = Struct(
     "mipmapCount" / Rebuild(Int16ul, lambda ctx: len(ctx.images)),
     "pixelFormatName" / Aligned(7, PascalString(Int16ul, "utf-8")),
     "checksum" / Bytes(16),
-    "offsets" / Rebuild(Array(this.mipmapCount, Int32ul), lambda ctx: [0,0,0,0,0,0]),
+    "offsets" / Rebuild(Array(this.mipmapCount, Int32ul), lambda ctx: [ctx._checksum_meta._endoffset + 2*ctx.mipmapCount*4 + sum([align(s, 4) for s in ctx.sizes[0:x]]) for x in range(ctx.mipmapCount)]),
     "sizes" / Rebuild(Array(this.mipmapCount, Int32ul), lambda ctx: [len(ctx.images[x].data) for x in range(ctx.mipmapCount)]),
-    "images" / Array(this.mipmapCount, "mipmap" / Pointer(lambda ctx: ctx.offsets[ctx._index], Struct("data" / Bytes(lambda ctx: ctx._.sizes[ctx._._index])))),
+    "images" / Array(this.mipmapCount, "mipmap" / Pointer(lambda ctx: ctx.offsets[ctx._index], Struct("data" / Aligned(4, Bytes(lambda ctx: ctx._.sizes[ctx._._index]))))),
     )
 
 
